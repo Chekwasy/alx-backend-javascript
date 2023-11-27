dcab3418827ebdae95c56f45b23a7eba2ea0fb39
Chekwasy <richardchekwas@gmail.com>
2023-11-27 22:22:15 +0100

    gg

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 /* the function */
-
 export default function taskBlock(trueOrFalse) {
     var task = false;
     var task2 = true;
+
     if (trueOrFalse) {
 	const task = true;
 	const task2 = false;
     }
+
     return [task, task2];
 }
