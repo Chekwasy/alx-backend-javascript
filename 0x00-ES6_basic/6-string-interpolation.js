8ff7b344af6a3cf9df2b1526562f14b972fbd152
Chekwasy <richardchekwas@gmail.com>
2023-11-29 19:25:04 +0100

    aa

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -5,5 +5,5 @@ export default function getSanFranciscoDescription() {
     gdp: '$154.2 billion',
     capita: '$178,479',
   };
-  return `As of ${String(year)}, it was the seventh-highest income county in the United States, with a per capita personal income of ${String(budget.income)}. As of 2015, San Francisco proper had a GDP of ${String(budget.gdp)}, and a GDP per capita of ${String(budget.capita)}.`;
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
