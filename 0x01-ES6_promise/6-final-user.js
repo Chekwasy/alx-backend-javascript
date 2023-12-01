fd6b404ede0a6a3c8bb585ed2c16cf8b97afe7e3
Chekwasy <richardchekwas@gmail.com>
2023-12-01 22:13:40 +0100

    aa

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,8 +3,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const lst = [];
-  lst.push(signUpUser(fileName, lastName));
-  lst.push(uploadPhoto(fileName));
-  return lst;
+  const pr1 = signUpUser(fileName, lastName);
+  const pr2 = uploadPhoto(fileName);
+  return Promise.allSettled([pr1, pr2]).then((response) => {
+    response.forEach((item) => {
+      const lst = [];
+      if (item.status === 'fulfiled') {
+        lst.push({ status: item.status, value: item.value });
+      } else {
+        lst.push({ status: item.status, value: `${item.reason}` });
+      }
+      return lst;
+    });
+  });
 }
