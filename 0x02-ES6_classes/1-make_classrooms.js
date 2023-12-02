7451cf59473a11ab5d900c440315f63ed1763331
Chekwasy <richardchekwas@gmail.com>
2023-12-02 21:13:31 +0100

    aa

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,12 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const arr = [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
-  return (arr);
+  const lst = [];
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+  lst.push(room1);
+  lst.push(room2);
+  lst.push(room3);
+  return lst;
 }
