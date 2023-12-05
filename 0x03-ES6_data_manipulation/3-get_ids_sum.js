c3c2e5370ed09ac64bdd18d11851b79b95a031fd
Chekwasy <richardchekwas@gmail.com>
2023-12-05 11:20:50 +0100

    aa

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,5 +2,8 @@
 export default function getStudentIdsSum(arr) {
   const lst = arr.map((arr) => (arr.id));
   const bg = 0;
-  return lst.reduce((acc, bg) => (acc + bg));
+  if (bg === 0) {
+    return lst.reduce((acc, bg) => (acc + bg));
+  }
+  return 1;
 }
