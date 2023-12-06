89c09b94b9ae55f197f26e4f2eef6b831cac89d6
Chekwasy <richardchekwas@gmail.com>
2023-12-06 01:32:26 +0100

    aa

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,14 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0) {
+  if (startString.length === 0 || startString === undefined) {
     return '';
   }
   const touse = [...set].filter((fil) => (fil.startsWith(startString)));
-  const fns = [];
-  for (const ab of touse) {
-    fns.push(ab.slice(startString.length));
+  if (touse.length !== 0) {
+    const fns = [];
+    for (const ab of touse) {
+      fns.push(ab.slice(startString.length));
+    }
+    return fns.join('-');
   }
-  return fns.join('-');
+  return '';
 }
