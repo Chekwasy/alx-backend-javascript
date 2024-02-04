da9a2fff245d5a58f796a8ed4a1d9f63f9faa92e
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2024-02-04 21:27:17 +0100

    aa

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,37 +1,37 @@
 /**
  * read a file and run some work on the file contents
- */ 
+ */
 const filechk = require('fs');
 
 const countStudents = (path) => {
-    if (!filechk.existsSync(path)) {
-	throw new Error('Cannot load the database');
+  if (!filechk.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  if (!filechk.statSync(path).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  const readed = filechk.readFileSync(path, 'utf-8').split('\n');
+  const len = readed.length - 1;
+  const allStd = [];
+  for (const std of readed.slice(1)) {
+    allStd.push(std.split(','));
+  }
+  const cs = [];
+  const swe = [];
+  for (const ech of allStd) {
+    if (ech.includes('CS')) {
+      cs.push(ech[0]);
     }
-    if (!filechk.statSync(path).isFile()) {
-	throw new Error('Cannot load the database');
+    if (ech.includes('SWE')) {
+      swe.push(ech[0]);
     }
-    const readed = filechk.readFileSync(path, 'utf-8').split('\n');
-    const len = readed.length - 1;
-    const all_std = [];
-    for (const std of readed.slice(1)) {
-	all_std.push(std.split(','));
-    }
-    const cs = []
-    const swe = []
-    for (const ech of all_std) {
-	if (ech.includes('CS')) {
-	    cs.push(ech[0]);
-	}
-	if (ech.includes('SWE')) {
-	    swe.push(ech[0]);
-	}
-    }
-    console.log(`Number of students: ${ len }`);
-    console.log(
-	`Number of students in CS: ${ cs.length }. List: ${ cs.join(', ') }`
-    );
-    console.log(
-	`Number of students in SWE: ${ swe.length }. List: ${ swe.join(', ') }`
-    );
-}
+  }
+  console.log(`Number of students: ${len}`);
+  console.log(
+    `Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`,
+  );
+  console.log(
+    `Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`,
+  );
+};
 module.exports = countStudents;
