6801c8b3e00509587899a942391eb541a9ef0568
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2024-02-07 14:40:30 +0100

    aa

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -9,45 +9,42 @@ const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
 
 const countStudents = (path) => new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-	if (err) {
-	    reject(new Error('Cannot load the database'));
-	}
-	if (data) {
-	    const readed = data.split('\n');
-	    const allStd = [];
-	    for (const std of readed.slice(1)) {
-		allStd.push(std.split(','));
-	    }
-	    const cs = [];
-	    const swe = [];
-	    for (const ech of allStd) {
-		if (ech.includes('CS')) {
-		    cs.push(ech[0]);
-		}
-		if (ech.includes('SWE')) {
-		    swe.push(ech[0]);
-		}
-	    }
-	    const len = cs.length + swe.length;
-	    const allM = [];
-	    allM.push(`Number of students: ${len}`);
-	    allM.push(
-		`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`,
-	    );
-	    allM.push(
-		`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`,
-	    );
-	    resolve(allM.join('\n'));
-	}
-    });
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    if (data) {
+      const readed = data.split('\n');
+      const allStd = [];
+      for (const std of readed.slice(1)) {
+        allStd.push(std.split(','));
+      }
+      const cs = [];
+      const swe = [];
+      for (const ech of allStd) {
+        if (ech.includes('CS')) {
+          cs.push(ech[0]);
+        }
+        if (ech.includes('SWE')) {
+          swe.push(ech[0]);
+        }
+      }
+      const len = cs.length + swe.length;
+      const allM = [];
+      allM.push(`Number of students: ${len}`);
+      allM.push(
+        `Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`,
+      );
+      allM.push(
+        `Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`,
+      );
+      resolve(allM.join('\n'));
+    }
+  });
 });
-
-
 
 const SERVER_ROUTE_HANDLERS = [
   {
