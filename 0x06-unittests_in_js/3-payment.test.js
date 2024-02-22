cb88f71a27a9e2f47b525419474b1e7db19b3f38
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2024-02-22 09:02:07 +0100

    aa

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,29 +1,15 @@
 const sinon = require('sinon');
+const Utils = require('./utils');
 const { expect } = require('chai');
-const sendPaymentRequestToApi = require('./5-payment');
+const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  let bigBrother;
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const bigBrother = sinon.spy(Utils);
 
-  beforeEach(() => {
-    if (!bigBrother) {
-      bigBrother = sinon.spy(console);
-    }
-  });
-
-  afterEach(() => {
-    bigBrother.log.resetHistory();
-  });
-
-  it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.log.calledWith('The total is: 120')).to.be.true;
-    expect(bigBrother.log.calledOnce).to.be.true;
-  });
-
-  it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
-    sendPaymentRequestToApi(10, 10);
-    expect(bigBrother.log.calledWith('The total is: 20')).to.be.true;
-    expect(bigBrother.log.calledOnce).to.be.true;
+    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
+    bigBrother.calculateNumber.restore();
   });
 });
